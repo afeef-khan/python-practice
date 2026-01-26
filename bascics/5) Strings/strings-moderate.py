9ab8b880c73680b98633b32c3794682a7da7f9d9
afeef-khan <afeefkhan95413@gmail.com>
2026-01-26 12:34:01 +0530

    Ignore!, just for setup purpose!!!

--- bascics/5) Strings/strings-moderate.py
+++ bascics/5) Strings/strings-moderate.py
@@ -1,43 +1,43 @@
 # q1. Count the number of vowels and consonants in a string.
 
-# input = input("Enter a string : ").lower()
+input = input("Enter a string : ").lower()
 
-# vowels = "aeiou"
-# vowelCount = 0
-# constCount = 0
+vowels = "aeiou"
+vowelCount = 0
+constCount = 0
 
-# for char in input:
-#     if char.isalpha(): #isaplha doesnt count spaces and special chars as consonanats
-#         if char in vowels :
-#             vowelCount += 1
-#         else:
-#             constCount += 1
+for char in input:
+    if char.isalpha(): #isaplha doesnt count spaces and special chars as consonanats
+        if char in vowels :
+            vowelCount += 1
+        else:
+            constCount += 1
 
-# print(f"There were {vowelCount} vowels and {constCount} constants")
+print(f"There were {vowelCount} vowels and {constCount} constants")
 
 # -----------------------------------------------------------------------------------------
 
 # q2. Reverse a string without using slicing.
 
-# str = input("Enter a string : ")
-# rev = ""
+str = input("Enter a string : ")
+rev = ""
 
-# for ch in str:
-#     rev = ch + rev
+for ch in str:
+    rev = ch + rev
 
-# print(f"Reverse of {str} is {rev}")
+print(f"Reverse of {str} is {rev}")
 
 # ----------------------------------------------------------------------------------------------------
 
 # # q3. Check whether a string is a palindrom. 
 
-# str = input("Enter a string : ")
-# rev = ""
+str = input("Enter a string : ")
+rev = ""
 
-# for ch in str:
-#     rev = ch + rev
+for ch in str:
+    rev = ch + rev
 
-# if str == rev:
-#     print(f"{str} is a palindrome")
-# else:
-#     print("Not a palindrome")
+if str == rev:
+    print(f"{str} is a palindrome")
+else:
+    print("Not a palindrome")
