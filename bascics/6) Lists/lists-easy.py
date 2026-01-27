b8eacf858780f84dec555290323668d204a25f56
afeef-khan <afeefkhan95413@gmail.com>
2026-01-27 11:16:07 +0530

    Lists easy, moderate 1, 3

---
+++ bascics/6) Lists/lists-easy.py
@@ -0,0 +1,36 @@
+# q1. Create a list of 5 integers and print the sum of all elements.
+
+# answer 1.
+# l = [1, 2, 3, 4, 5]
+# print(sum(l))
+
+# answer 2.
+# numbers = [1, 2, 3, 4, 10]
+# total = 0
+
+# for n in numbers:
+#     total += n
+
+# print(f"Total = {total}")
+
+# ------------------------------------------------------------------------------
+
+# q2. Write a program to find the largest element in a list without using max().
+
+# l = [1, 2, 3, 40, 5]
+# largest = l[0]
+
+# for n in l:
+#     if n > largest:
+#         largest = n
+
+# print(largest)
+
+# ----------------------------------------------------------------------------------
+
+# q3. Given a list, print all elements at even indexes. 
+
+# l = [1, 2, 3, 4, 5, 6]
+
+# for n in range(0, len(l), 2):
+#     print(l[n])
---
+++ bascics/6) Lists/lists-moderate.py
@@ -0,0 +1,30 @@
+# q1. Reverse a list without using reverse() or slicing.
+
+# l = [1, 2 ,3 ,4, 5]
+# rev = []
+
+# for i in range(len(l) - 1, -1, -1):
+#     rev.append(l[i])
+
+# print(rev)
+
+# ---------------------------------------------------------------------------------------------------------
+
+# q2. Count how many times each element appears in a list.
+
+# ---------------------------------------------------------------------------------------------
+
+# q3. Separate even and odd numbers from a list into two different lists.
+
+# l = [1, 2, 3, 4]
+
+# even = []
+# odd = []
+
+# for n in l:
+#     if n % 2 == 0:
+#         even.append(n)
+#     else:
+#         odd.append(n)
+
+# print(f"even = {even}, odd = {odd}")
