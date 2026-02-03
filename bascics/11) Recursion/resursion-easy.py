9fb5f76e2e39c7d07ed4068f2498eb804d6b869c
afeef-khan <afeefkhan95413@gmail.com>
2026-02-03 21:28:47 +0530

    Recursion easy, moderate 1

---
+++ bascics/11) Recursion/resursion-easy.py
@@ -0,0 +1,31 @@
+# q. Write a recursive function to print numbers from 1 to n.
+
+# def counter(n):
+#     if n == 0:
+#         return
+#     counter(n - 1)
+#     print(n)
+
+# counter(10)
+
+# ---------------------------------------------------------------------------
+
+# q. Write a recursive function to calculate the sum of first n natural numbers.
+
+# def addition(x):
+#     if x == 0:
+#         return 0
+#     return x + addition(x - 1)
+
+# print(addition(6))
+
+# -----------------------------------------------------
+
+# q. Write a recursive function to calculate factorial of a number.
+
+# def fact(x):
+#     if x == 0 or x ==1:
+#         return 1
+#     return x * fact(x -1)
+
+# print(fact(3))
---
+++ bascics/11) Recursion/resursion-moderate.py
@@ -0,0 +1,14 @@
+# q. Write a recursive function to reverse a string.
+
+# def revStr(x):
+#     if x == "":
+#         return ""
+#     return revStr(x[1:]) + x[0]
+
+# print(revStr("afeef"))
+
+# -----------------------------------------------------------
+
+# q. Write a recursive function to check whether a string is a palindrome.
+
+# q. Write a recursive function to calculate the nth Fibonacci number.
